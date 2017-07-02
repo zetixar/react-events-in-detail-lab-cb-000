369ca082e390e502b359f4fd1c858c3bfd7dd238
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-07-02 15:49:21 +0000

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,15 @@
-// Code CoordinatesButton Component Here
+import React from 'react'
+
+export default class CoordinatesButton extends React.Component {
+  onDelayedClick = (event) => {
+    this.props.onReceiveCoordinates([event.clientX, event.clientY]);
+    // console.log([event.clientX, event.clientY]);
+  }
+
+  render(){
+    return(
+      <button onClick={this.onDelayedClick} >coordinates button</button>
+    );
+  };
+
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,27 @@
-// Code DelayedButton Component Here
+import React from 'react'
+
+export default class DelayedButton extends React.Component {
+  delayedClick = (event) => {
+    event.persist();
+    setTimeout(() => {this.props.onDelayedClick(event)}, this.props.delay);
+    // console.log(this.props.delay);
+  }
+  render(){
+    return(
+      <button onClick={this.delayedClick} >delayed click button</button>
+    );
+  };
+
+  // handleClick (e){
+  //   console.log('The event currentTarget is', e.currentTarget); // DOM element
+  //   debugger;
+  //   setTimeout(() => {
+  //     console.log('event.currentTarget was', e.currentTarget); // null
+  //     }, 1000)
+  //   }
+  //   debugger;
+  //
+  //   render () {
+  //     return <button onClick={this.handleClick}>Fire event!</button>
+  //   }
+}
